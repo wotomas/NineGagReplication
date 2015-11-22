818c6dbae444fcc10e3d50dbedf6c98366a452c9
Jimmy Kim <jkimab@ust.hk>
2015-11-23 00:45:52 +0800

    Added several arraylists for various lists
    Added several arraylists for various lists

--- NineGagOfflineTest/app/src/main/java/com/lohasfarm/kim/ninegagofflinetest/unitstorage/GagStorage.java
+++ NineGagOfflineTest/app/src/main/java/com/lohasfarm/kim/ninegagofflinetest/unitstorage/GagStorage.java
@@ -1,81 +1,149 @@
 package com.lohasfarm.kim.ninegagofflinetest.unitstorage;
 
 import android.content.Context;
+import android.util.Log;
 
 import com.google.gson.Gson;
 import com.lohasfarm.kim.ninegagofflinetest.unit.Gag;
 import com.lohasfarm.kim.ninegagofflinetest.unitstorage.disk.FileManager;
 import com.lohasfarm.kim.ninegagofflinetest.unitstorage.disk.JsonStorable;
 
-import java.util.LinkedList;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 
 /**
  * Created by kim on 2015-11-18.
  */
 public class GagStorage implements JsonStorable{
-    private LinkedList<Gag> _gagList;
-    //private LinkedList<Gag> _gagSortedThroughTime;
-    //private LinkedList<Gag> _gagSortedThroughTrend;
-    //private int _lastLoadedGagNumber;
+    //hot
+    private ArrayList<Gag> _gagList;
+    //fresh
+    private ArrayList<Gag> _gagSortedThroughTime;
+    //trending
+    private ArrayList<Gag> _gagSortedThroughComments;
+    //top
+    private ArrayList<Gag> _gagSortedThroughPoints;
+
+    //private int[]
+    //0 - Horizontal List loaded Number
+    //1 - Vertical List loaded Number
+    //2 - Trending List loaded Number
+    //3 - Fresh List loaded Number
+    //4 - Top List loaded Number
+    private int[] _lastLoadedGagNumber = {0,0,0,0,0};
 
     private int _gagNumber = 0;
 
     public GagStorage() {
-        _gagList = new LinkedList<Gag>();
-        //_gagSortedThroughTime = doInsertionSort(_gagList, "time");
-        //_gagSortedThroughTrend = doInsertionSort(_gagList, "trend");
+        _gagList = new ArrayList<Gag>();
+        _gagSortedThroughTime = new ArrayList<Gag>();
+        _gagSortedThroughComments = new ArrayList<Gag>();
+        _gagSortedThroughPoints = new ArrayList<Gag>();
     }
 
-   // public int get_lastLoadedGagNumber() {
-        //return _lastLoadedGagNumber;
-   // }
+    public void sortAccordingly(Context context) {
+        ArrayList<Gag> sample = new ArrayList<Gag>();
+        sample.addAll(_gagList);
+        _gagList = doMergeSort(sample, "id");
 
-    public LinkedList<Gag> getEntireList() {
-        return _gagList;
+        _gagSortedThroughTime = doMergeSort(sample, "time");
+        //Log.d("SortingTest", "Sorted Through time: " + _gagSortedThroughTime.toString());
+        _gagSortedThroughComments = doMergeSort(sample, "comments");
+        //Log.d("SortingTest", "Sorted Through comments: " + _gagSortedThroughComments.toString());
+        _gagSortedThroughPoints = doMergeSort(sample, "points");
+        saveToJson(context);
+        //Log.d("SortingTest", "Sorted Through points: " + _gagSortedThroughPoints.toString());
+
     }
 
-    public LinkedList<Gag> get_gagList(int start, int end) {
-        LinkedList<Gag> output = new LinkedList<Gag>();
+    public int get_totalSIze() {
+        return _gagNumber;
+    }
+
+    public int get_lastLoadedGagNumber(int number) {
+        return _lastLoadedGagNumber[number];
+    }
+
+    public ArrayList<Gag> get_gagList(int type, int start, int end) {
+        Log.d("FragmentCheck", "Input fragment getlist is " + Integer.toString(type));
+        ArrayList<Gag> output = new ArrayList<Gag>();
         int counter = start;
-        for(Gag gag : _gagList){
-            if(counter == _gagList.size()) {
+        switch(type) {
+            case 0:
+                Log.d("FragmentCheck", "Get From normal list");
+                for(Gag gag : _gagList){
+                    if(counter == _gagList.size()) {
+                        break;
+                    }
+                    output.add(gag);
+                    counter++;
+                    _lastLoadedGagNumber[type] = counter;
+                    if(counter > end) {
+                        break;
+                    }
+                }
                 break;
-            }
-            output.add(counter, gag);
-            counter++;
-            if(counter > end) {
+            case 1:
+                Log.d("FragmentCheck", "Get From vertical list");
+                for(Gag gag : _gagSortedThroughComments){
+                    if(counter == _gagSortedThroughComments.size()) {
+                        break;
+                    }
+                    output.add(gag);
+                    counter++;
+                    _lastLoadedGagNumber[type] = counter;
+                    if(counter > end) {
+                        break;
+                    }
+                }
                 break;
-            }
+            case 2:
+                Log.d("FragmentCheck", "Get From Point list");
+                for(Gag gag : _gagSortedThroughComments){
+                    if(counter == _gagSortedThroughComments.size()) {
+                        break;
+                    }
+                    output.add(gag);
+                    counter++;
+                    _lastLoadedGagNumber[type] = counter;
+                    if(counter > end) {
+                        break;
+                    }
+                }
+                break;
+            case 3:
+                Log.d("FragmentCheck", "Get From Comments list");
+                for(Gag gag : _gagSortedThroughTime){
+                    if(counter == _gagSortedThroughTime.size()) {
+                        break;
+                    }
+                    output.add(gag);
+                    counter++;
+                    _lastLoadedGagNumber[type] = counter;
+                    if(counter > end) {
+                        break;
+                    }
+                }
+                break;
+            case 4:
+                Log.d("FragmentCheck", "Get From Time list");
+                for(Gag gag : _gagSortedThroughPoints){
+                    if(counter == _gagSortedThroughPoints.size()) {
+                        break;
+                    }
+                    output.add(gag);
+                    counter++;
+                    _lastLoadedGagNumber[type] = counter;
+                    if(counter > end) {
+                        break;
+                    }
+                }
+                break;
         }
-        //_lastLoadedGagNumber = end;
-        return output;
-    }
 
-    /**
-    public LinkedList<Gag> get_gagSortedThroughTime(int number) {
-        LinkedList<Gag> output = new LinkedList<Gag>();
-        int counter = 0;
-        for(Gag gag : _gagSortedThroughTime) {
-            output.add(counter, _gagSortedThroughTime.get(counter));
-            if(counter == number) {
-                break;
-            }
-        }
         return output;
     }
-
-    public LinkedList<Gag> get_gagSortedThroughTrend(int number) {
-        LinkedList<Gag> output = new LinkedList<Gag>();
-        int counter = 0;
-        for(Gag gag : _gagSortedThroughTrend) {
-            output.add(counter, _gagSortedThroughTrend.get(counter));
-            if(counter == number) {
-                break;
-            }
-        }
-        return output;
-    }
-    **/
 
     public void addGag(Gag gag, Context context) {
         _gagList.add(gag);
@@ -83,39 +151,47 @@ public class GagStorage implements JsonStorable{
         saveToJson(context);
     }
 
-    private LinkedList<Gag> doInsertionSort(LinkedList<Gag> gagList, String type) {
-        //use insertion sort because
-        //1. there is high possibility that time is already sorted
-        // ex) new posts are added into bottom, so insertion should run from bottom
-        //2. stable, but becomes inefficient in large lists
+    private ArrayList<Gag> doMergeSort(ArrayList<Gag> gagList, String type) {
+        //use MergeSort sort because
+        //1. Mergesort works even better on linked lists than it does on arrays
+        //2. Stable
+        //3. guaranteed n Log n performance
+        ArrayList<Gag> output = new ArrayList<Gag>();
+        output.addAll(gagList);
+        //Log.d("Sorting Test", "output before: " + output.toString());
         if(type.equals("time")) {
-            Gag temp = new Gag();
-            for(int i = 1; i < gagList.size(); i++) {
-                for(int j = 1; j > 0; j--) {
-                    if(gagList.get(j).get_postDate().getTime() < gagList.get(j-1).get_postDate().getTime() ) {
-                        //if time is larger, it means more time has past since 1970,
-                        //therefore its a more recent post
-                        temp = gagList.get(j);
-                        gagList.set(j, gagList.get(j-1));
-                        gagList.set(j-1, temp);
-                    }
+            Collections.sort(output, new Comparator<Gag>() {
+                @Override
+                public int compare(Gag lhs, Gag rhs) {
+                    return rhs.get_postDate().compareTo(lhs.get_postDate());
                 }
-            }
+            });
+            Log.d("Sorting Test", "Sort Once!");
+        } else if(type.equals("points")) {
+            Collections.sort(output, new Comparator<Gag>() {
+                @Override
+                public int compare(Gag lhs, Gag rhs) {
+                    return rhs.get_points() - lhs.get_points();
+                }
+            });
+            Log.d("Sorting Test", "Sort Twice!");
+        } else if(type.equals("comments")){
+            Collections.sort(output, new Comparator<Gag>() {
+                @Override
+                public int compare(Gag lhs, Gag rhs) {
+                    return rhs.get_comments() - lhs.get_comments();
+                }
+            });
+            Log.d("Sorting Test", "Sort Thrice!");
         } else {
-            Gag temp = new Gag();
-            for(int i = 1; i < gagList.size(); i++) {
-                for(int j = 1; j > 0; j--) {
-                    if(gagList.get(j).get_points() < gagList.get(j-1).get_points() ) {
-                        //if time is larger, it means more time has past since 1970,
-                        //therefore its a more recent post
-                        temp = gagList.get(j);
-                        gagList.set(j, gagList.get(j-1));
-                        gagList.set(j-1, temp);
-                    }
+            Collections.sort(output, new Comparator<Gag>() {
+                @Override
+                public int compare(Gag lhs, Gag rhs) {
+                    return lhs.get_id() - rhs.get_id();
                 }
-            }
+            });
         }
-        return gagList;
+        return output;
     }
 
     @Override
@@ -142,7 +218,6 @@ public class GagStorage implements JsonStorable{
         Gson gson = new Gson();
         FileManager.getInstance().writeToFile(gson.toJson(this), getFileName(), context);
     }
-
 
 
 }
